7152af15d08d76692a04ad75de9c68ddda8ffdb9
van chung chen <auxo_86@hotmail.com>
2017-05-28 16:05:57 +0800

    加入專案檔案。

---
+++ MODULE04_09/Models/ProductSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MODULE04_09.DAL;
+//資料從ViewModels來，不是從Models
+using MODULE04_09.ViewModels;
+
+namespace MODULE04_09.Models
+{
+    public class ProductSystem
+    {
+        //public Product GetProductByID(int id)
+        //{
+        //    NorthwindEntities db = new NorthwindEntities();
+        //    var query = from p in db.Products
+        //                where p.ProductID == id
+        //                select p;
+
+        //    //只列第一筆
+        //    var result = query.First();
+
+        //    return result;
+
+        //}
+
+        public ProductViewModel GetProductByID(int id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var query = from p in db.Products
+                        where p.ProductID == id
+                        select new ProductViewModel
+                        {
+                            ProductID=p.ProductID,
+                            ProductName=p.ProductName,
+                            UnitPrice=p.UnitPrice
+                        };
+
+            //只列第一筆
+            var result = query.First();
+
+            return result;
+        }
+
+        public IEnumerable<Product> GetProductsByCategoryID(int id)
+        {
+            NorthwindEntities db = new NorthwindEntities();
+            var query = from p in db.Products
+                        where p.CategoryID == id
+                        select p;
+
+            var result = query.ToList();
+            return result;
+        }
+
+
+    }
+}
